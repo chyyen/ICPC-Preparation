9fcca4f1dc3680add7aefc91f46cedffdb9d6cd7
ljh-coding <frankleeplayminecraft58@gmail.com>
2023-11-14 03:49:00 +0800

    update ljh vimrc

---
+++ Codebook/Setup/vimrc2.h
@@ -0,0 +1,6 @@
+se nu ai hls et ru ic is sc cul
+se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+syntax on
+hi cursorline cterm=none ctermbg=89
+set bg=dark
+map <F5> :w <CR> :!clear ; g++ -g --std=c++17 % && echo Compiled successfully. && ./a.out; <CR>
